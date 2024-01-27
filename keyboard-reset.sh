437040c5e3c5cd7495737eb3f3efedbc5c30dbd5
Frank Loesche <loesche@tamotua.de>
2024-01-27 11:47:16 -0500

    add USB reset

--- keyboard-reset.sh
+++ keyboard-reset.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#! /bin/sh
+## Add USB reset from https://bbs.archlinux.org/viewtopic.php?pid=2017609#p2017609
 case $1/$2 in
-    pre/*)
-        rmmod i8042
-        ;;
-    post/*)
-        modprobe i8042 reset=1
-        ;;
+  pre/*)
+    rmmod i8042
+    echo "remove i8042"
+    for USB in /sys/bus/usb/devices/*/authorized; do
+        eval "echo 0 > $USB" 2>/dev/null
+    done
+    echo "turn off usb"
+    ;;
+  post/*)
+    modprobe i8042 reset=1
+    echo "reset keyboard"
+    for USB in /sys/bus/usb/devices/*/authorized; do
+        eval "echo 1 > $USB" 2>/dev/null
+    done
+    echo "turn on usb"
+    ;;
 esac
